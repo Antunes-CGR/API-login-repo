a0d6d6d12966449875da59383e360b3b6642cbb9
João Paulo <joaoantunees@gmail.com>
2023-03-04 11:06:08 -0300

    feat: inclusão de rota delete na task

--- app.js
+++ app.js
@@ -124,36 +124,57 @@ app.post("/auth/login", async (req, res) => {
 });
 
 app.get("/tasks", checkToken, async (req, res) => {
-  const [ ,token] = req.headers.authorization.split(' ')
-  const {id: user_id} = jwt.decode(token)
+  const [, token] = req.headers.authorization.split(" ");
+  const { id: user_id } = jwt.decode(token);
 
-  const tasks = await Task.find({ user_id })
+  const tasks = await Task.find({ user_id });
   return res.status(200).json(tasks);
 });
 
-// app.post("/tasks/", async (req, res) => {
+app.post("/tasks", async (req, res) => {
+  const [, token] = req.headers.authorization.split(" ");
+  const { id: user_id } = jwt.decode(token);
 
-//   res.status(201).json(task);
-// });
+  const TaskCreate = await Task.create({
+    user_id: user_id,
+    titulo: req.body.titulo,
+    created_at: new Date(),
+    completed_at: null,
+  });
+  return res.status(201).json(TaskCreate);
+});
 
-// app.delete("/tasks/destroy/:id", async (req, res) => {
-//   const { user_id } = req.header
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const [, token] = req.headers.authorization.split(" ");
+    const { id: user_id } = jwt.decode(token);
+    const id = req.params.id;
 
-//   if(user_id === user_id){
-//     await user_id.findByIdAndDelete({ _id: user_id})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ msg: "task id inválido" });
+    }
 
-//     return res.status(200).json({msg:'deletado'})
-//   } else {
+    const task = await Task.findOneAndDelete({ user_id, _id: id });
 
-//     return res.status(403).json({msg:'erro'})
-//   }
-// })
-// app.patch('/tasks/:id', async (req, res) => {
-//   const task = {}
-  
-//   res.status(202).json(task)
+    console.log(task);
 
-// });
+    if (!task) {
+      return res.status(404).json({ msg: "task inexistente" });
+    }
+
+    return res.status(200).json({ msg: `Task ${task.titulo} removida` });
+  } catch (err) {
+    return res.status(403).json({ msg: err.message });
+  }
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  const [, token] = req.headers.authorization.split(" ");
+  const { id: user_id } = jwt.decode(token);
+
+  return res.status(200).json(finalTask);
+});
+
 //Credenciais
 
 const dbUser = process.env.DB_USER;
