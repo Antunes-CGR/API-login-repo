a4a821684731c47253b91373bd37f23276adab9c
João Paulo <joaoantunees@gmail.com>
2023-03-22 18:31:22 -0300

    chore: aplicação de Controller para Task.

--- app.js
+++ app.js
@@ -17,9 +17,11 @@ const { bookStoreValidator } = require("./middlewares/BookValidator/bookStore_va
 // models
 const User = require("./models/User")
 const Task = require("./models/Task")
+
 // controllers
 const ControllerBook = require("./controller/ControllerBook")
 const ControllerAuth = require("./controller/ControllerAuth")
+const ControllerTask = require("./controller/ControllerTask")
 
 const app = express()
 app.use(express.json())
@@ -38,93 +40,14 @@ app.post("/auth/register", registerValidator, ControllerAuth.postUser)
 // Login User
 app.post("/auth/login", loginValidator, ControllerAuth.loginUser)
 
+// Rotas Controller Task
+app.post("/tasks", checkToken, validarFormTask, ControllerTask.postTask)
+app.delete("/tasks/:id", checkToken, taskDeleteValidator, ControllerTask.destroyTask)
+app.put("/tasks/:_id", checkToken, taskUpdateValidator, ControllerTask.putTask)
+app.put("/tasks/:_id/completed", checkToken, ControllerTask.putTaskCompleted)
+
 // paginação task
-app.get("/tasks", checkToken, async (req, res) => {
-  const { user_id } = res.locals
-  
-  const {page, limit} = req.query
-  const skip = (page - 1) * limit //Conta fixa para paginação
-  //const sort = ({ titulo: -1 })
-
-  const tasks = await Task.find({ user_id }, null, {limit, skip}) // Paginação
-
-  const totalTask = await Task.count({user_id}) //critério de contagem
-  const totalPages = Math.ceil(totalTask / limit) // Mostrar limite de paginas ao usuário
-
-  return res.status(200).json({tasks, totalPages})
-})
-
-app.post("/tasks", checkToken, validarFormTask, async (req, res) => {
-  const { user_id } = res.locals
-
-  const TaskCreate = await Task.create({
-    user_id,
-    titulo: req.body.titulo,
-    created_at: new Date(),
-    completed_at: null,
-  })
-
-  return res.status(201).json(TaskCreate)
-})
-
-app.delete("/tasks/:id", checkToken, taskDeleteValidator, async (req, res) => {
-  try {
-    const { user_id } = res.locals
-    const id = req.params.id
-
-    const task = await Task.findOneAndDelete({ user_id, _id: id })
-
-    console.log(task)
-    //validation
-    if (!task) {
-      return res.status(404).json({ msg: "task inexistente" })
-    }
-
-    return res.status(200).json({ msg: `Task ${task.titulo} removida` })
-  } catch (err) {
-    return res.status(403).json({ msg: err.message })
-  }
-})
-
-app.put("/tasks/:_id", checkToken, taskUpdateValidator, async (req, res) => {
-  try {
-
-    const { user_id } = res.locals
-    const { _id } = req.params
-
-    const taskDoUsuario = await Task.findOne({ _id, user_id })
-
-    taskDoUsuario.titulo = req.body.titulo
-    await taskDoUsuario.save()
-
-    return res.status(200).json(taskDoUsuario)
-  } catch (erro) {
-    console.log(erro)
-  }
-})
-
-app.put("/tasks/:_id/completed", checkToken, async (req, res) => {
-  try {
-    // validar se a task pertence ao usuario
-
-    const { _id } = req.params
-    const { user_id } = res.locals
-
-    const taskDoUsuario = await Task.findOne({ _id, user_id })
-
-    if (!taskDoUsuario) {
-      return res.status(404).json({ msg: "Usuário não autorizado" })
-    }
-
-    // const taskCompleted = await Task.findOneAndUpdate( {_id}, {completed_at: new Date()})
-
-    taskDoUsuario.completed_at = new Date()
-    await taskDoUsuario.save()
-    return res.status(200).json(taskDoUsuario)
-  } catch (error) {
-    console.log(error)
-  }
-})
+app.get("/tasks", checkToken, ControllerTask.getTask )
 
 //Rotas Controller Book
 app.get("/taskBook", ControllerBook.index)
@@ -137,7 +60,6 @@ app.get("/taskBook/:_id", ControllerBook.show)
 app.get("/Book/pag", checkToken, ControllerBook.list)
 
 //Credenciais
-
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
 mongoose.set("strictQuery", true)
---
+++ controller/ControllerTask.js
@@ -0,0 +1,92 @@
+
+
+//imports
+const Task = require("../models/Task")
+
+
+class ControllerTask {
+  async getTask (req, res) {
+    const { user_id } = res.locals
+  
+    const {page, limit} = req.query
+    const skip = (page - 1) * limit //Conta fixa para paginação
+    //const sort = ({ titulo: -1 })
+  
+    const tasks = await Task.find({ user_id }, null, {limit, skip}) // Paginação
+  
+    const totalTask = await Task.count({user_id}) //critério de contagem
+    const totalPages = Math.ceil(totalTask / limit) // Mostrar limite de paginas ao usuário
+  
+    return res.status(200).json({tasks, totalPages})
+  }
+  async postTask (req, res) {
+    const { user_id } = res.locals
+
+    const TaskCreate = await Task.create({
+      user_id,
+      titulo: req.body.titulo,
+      created_at: new Date(),
+      completed_at: null,
+    })
+  
+    return res.status(201).json(TaskCreate)
+  }
+  async putTask (req, res) {
+    try {
+
+      const { user_id } = res.locals
+      const { _id } = req.params
+  
+      const taskDoUsuario = await Task.findOne({ _id, user_id })
+  
+      taskDoUsuario.titulo = req.body.titulo
+      await taskDoUsuario.save()
+  
+      return res.status(200).json(taskDoUsuario)
+    } catch (erro) {
+      console.log(erro)
+    }
+  }
+  async putTaskCompleted (req, res) {
+    try {
+      // validar se a task pertence ao usuario
+  
+      const { _id } = req.params
+      const { user_id } = res.locals
+  
+      const taskDoUsuario = await Task.findOne({ _id, user_id })
+  
+      if (!taskDoUsuario) {
+        return res.status(404).json({ msg: "Usuário não autorizado" })
+      }
+  
+      // const taskCompleted = await Task.findOneAndUpdate( {_id}, {completed_at: new Date()})
+  
+      taskDoUsuario.completed_at = new Date()
+      await taskDoUsuario.save()
+      return res.status(200).json(taskDoUsuario)
+    } catch (error) {
+      console.log(error)
+    } 
+  }
+  async destroyTask (req, res) {
+    try {
+      const { user_id } = res.locals
+      const id = req.params.id
+  
+      const task = await Task.findOneAndDelete({ user_id, _id: id })
+  
+      console.log(task)
+      //validation
+      if (!task) {
+        return res.status(404).json({ msg: "task inexistente" })
+      }
+  
+      return res.status(200).json({ msg: `Task ${task.titulo} removida` })
+    } catch (err) {
+      return res.status(403).json({ msg: err.message })
+    }
+  }
+}
+
+module.exports = new ControllerTask()
